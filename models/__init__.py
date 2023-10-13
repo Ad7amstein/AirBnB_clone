ae0ceea77f5bf02fab238b729143221ee014d38c
Ad7amstein <adham32003200@gmail.com>
2023-10-13 17:46:51 +0300

    add cls names

--- models/__init__.py
+++ models/__init__.py
@@ -6,9 +6,16 @@
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
+from models.city import City
 
 
-cls_names = {"BaseModel": BaseModel, "User": User}
+cls_names = {"BaseModel": BaseModel, "User": User, "Place": Place,
+             "State": State, "City": City, "Amenity": Amenity,
+             "Review": Review}
 
 storage = FileStorage()
 storage.reload()
