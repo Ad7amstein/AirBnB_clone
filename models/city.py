c02c17768f22b33e61cd390e72e6a624fd2d0490
Ad7amstein <adham32003200@gmail.com>
2023-10-13 17:41:31 +0300

    add city model

---
+++ models/city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""Defines a city class
+"""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents a city model
+    """
+
+    state_id = ""
+    name = ""
