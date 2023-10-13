6bfe57078136df2f3cdcf0ebcb498d8ce4463f3f
Ad7amstein <adham32003200@gmail.com>
2023-10-13 17:43:14 +0300

    add amenity model

---
+++ models/amenity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines amenity class
+"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Represents an amenity model.
+    """
+
+    name = ""
