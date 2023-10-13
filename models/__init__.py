02ad9d97c3bfe603402fe658d9241a6a0876e953
Ad7amstein <adham32003200@gmail.com>
2023-10-13 15:10:33 +0300

    add cls_names

--- models/__init__.py
+++ models/__init__.py
@@ -4,6 +4,11 @@
 """
 
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+
+
+cls_names = {"BaseModel": BaseModel, "User": User}
 
 storage = FileStorage()
 storage.reload()
