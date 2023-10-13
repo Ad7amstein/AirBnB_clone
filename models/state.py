891adee1131054cb247023da66b5c3026e50d019
Ad7amstein <adham32003200@gmail.com>
2023-10-13 17:42:34 +0300

    add state model

---
+++ models/state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Defines a state class
+"""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represents a state class
+    """
+
+    name = ""
